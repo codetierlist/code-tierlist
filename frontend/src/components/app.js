6c5b1db828e42e12ded003c426f3315419723c27
logonoff <logonoff@users.noreply.github.com>
2023-04-29 03:05:36 -0400

    frontend: fix not build

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,31 +4,43 @@ import { Router } from 'preact-router';
 import Header from './header';
 import { GoogleTheme } from "./theme/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { userContext } from "../context/userContext";
+import { userContext } from "../contexts/UserContext";
+import { useState, useEffect } from 'preact/hooks';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 
-const userInfo = {
-	role: "student", // student, instructor
-	name: "John Doe",
-}
+const App = () => {
+	const [user, setUser] = useState({});
 
-const App = () => (
-	<ThemeProvider theme={GoogleTheme}>
-		<CssBaseline />
-		<div id="app">
-			<Header />
-			<main>
-				<Router>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
-			</main>
-		</div>
-	</ThemeProvider>
-);
+	useEffect(() => {
+		const data = {
+			loggedIn: true,
+			role: "student", // student, instructor
+			name: "John Doe",
+		}
+
+		setUser(data);
+	}, []);
+
+	return (
+		<ThemeProvider theme={GoogleTheme}>
+			<userContext.Provider value={user} />
+				<CssBaseline />
+				<div id="app">
+					<Header />
+					<main>
+						<Router>
+							<Home path="/" />
+							<Profile path="/profile/" user="me" />
+							<Profile path="/profile/:user" />
+						</Router>
+					</main>
+				</div>
+			<userContext.Provider />
+		</ThemeProvider>
+	);
+}
 
 export default App;
