052f3a1d4431560112653b4b948ef21fa3b4314c
Giang Bui <bhuonggiang03@gmail.com>
2023-04-29 05:49:40 -0400

    edit example data

--- backend/src/analytics/analytics.controller.ts
+++ backend/src/analytics/analytics.controller.ts
@@ -1,35 +1,4 @@
 import { Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
-import { Assignment } from 'src/types';
-
-interface AssignmentInfo {
-  name: string;
-  description: string;
-  numTests: number;
-}
 
 @Controller('analytics')
-export class AnalyticsController {
-  @Post('assignments')
-  @UseInterceptors(FilesInterceptor('files'))
-  async createAssignment(
-    @UploadedFiles() files: Express.Multer.File[],
-  ): Promise<Assignment> {
-    return {
-      name: 'assignment1',
-      description: 'assignment1 description',
-      solution: 'solution1 file',
-    };
-  }
-
-  @Get('assignments')
-  async getAssignments(): Promise<AssignmentInfo[]> {
-    return [
-      {
-        name: 'assignment1',
-        description: 'assignment1 description',
-        numTests: 2,
-      },
-    ];
-  }
-}
+export class AnalyticsController {}
--- backend/src/assignment/assignment.controller.ts
+++ backend/src/assignment/assignment.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { Assignment } from 'src/types';
+
+interface AssignmentInfo {
+  name: string;
+  description: string;
+  numTests: number;
+}
 
 @Controller('assignment')
-export class AssignmentController {}
+export class AssignmentController {
+  @Post('assignments')
+  @UseInterceptors(FilesInterceptor('files'))
+  async createAssignment(
+    @UploadedFiles() files: Express.Multer.File[],
+  ): Promise<Assignment> {
+    return {
+      name: 'CSC148-A1',
+      description: 'Introduction to suffering',
+      solution: 'solution1 file but actually a string',
+    };
+  }
+
+  @Get('assignments')
+  async getAssignments(): Promise<AssignmentInfo[]> {
+    return [
+      {
+        name: 'CSC148-A1',
+        description: 'Introduction to suffering',
+        numTests: 2,
+      },
+    ];
+  }
+}
